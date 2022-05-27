2b9ee980522d66aa75dadb8b31a6af81a1514ca7
Scarlettlp <scarlett.lara.p@gmail.com>
2022-05-27 18:24:19 -0500

    bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+            {" "}
+      Weather App
+      <div className="container">
+        <Weather/>
+        <footer>
+          Coded by Scarlett Lara
+        </footer>
+      </div>
+    </div>
+  )
+
 }
---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Weather(){
+    return(
+        <div className="Weather">
+        <h1>Java</h1>
+        <ul>
+            <li>Sunday 07:09</li>
+            <li>Mostly cloudy</li>
+        </ul>
+    </div>
+      
+    );
+}
