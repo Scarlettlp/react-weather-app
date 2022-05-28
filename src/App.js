126c0d65f7dc2d5dd433d2dbbf72ba021a960394
Scarlettlp <scarlett.lara.p@gmail.com>
2022-05-27 20:12:47 -0500

    failed to compile

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-            {" "}
+      {" "}
       Weather App
       <div className="container">
-        <Weather/>
-        <footer>
-          Coded by Scarlett Lara
-        </footer>
+        <Weather />
+        <footer>Coded by Scarlett Lara</footer>
       </div>
     </div>
-  )
-
+  );
 }
