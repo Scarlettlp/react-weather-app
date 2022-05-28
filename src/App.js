5ccfb6d73d696a67f6162bd6a877cc32837d069b
Scarlettlp <scarlett.lara.p@gmail.com>
2022-05-27 22:37:54 -0500

    last changes saved

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import "./App.css";
 import Weather from "./Weather";
 import React from "react";
 
-
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-       
         <Weather />
         <footer>
-        <a href="https://github.com/Scarlettlp/react-weather-app" target="_blank">Open souce coded by</a> Scarlett Lara </footer>
+          <a
+            href="https://github.com/Scarlettlp/react-weather-app"
+            target="_blank"
+          >
+            Open souce coded by
+          </a>{" "}
+          Scarlett Lara{" "}
+        </footer>
       </div>
     </div>
   );
